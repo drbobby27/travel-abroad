b93fd5db0492ec8a0366d78870c70f089919a83c
drbobby27 <envioshseq@gmail.com>
2023-02-01 16:42:05 -0500

    update

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -7,7 +7,7 @@ const TopBar = () => {
     <div className='flex justify-between items-center px-4 py-2'>
       <div className='flex items-center'>
         <BsChatSquareDots size={30} className='text-[var(--primary-dark)] mr-2'  />
-        <h1 className='text-xl font-bold text-gray-700'>WEEKAWAY</h1>
+        <h1 className='text-xl font-bold text-gray-700'>Drbobby Abroad</h1>
       </div>
       <div className='flex'>
         <div className='hidden md:flex items-center px-6'>
@@ -16,7 +16,7 @@ const TopBar = () => {
         </div>
         <div className='hidden md:flex items-center px-6'>
           <AiFillPhone size={20} className='mr-2 text-[var(--primary-dark)]' />
-          <p className='text-sm text-gray-700'>1-888-817-1234</p>
+          <p className='text-sm text-gray-700'>57-319-525-5624</p>
         </div>
         <button>Get a Free Quote</button>
       </div>
